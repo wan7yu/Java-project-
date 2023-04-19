180a0fcc643693eeaadbfb99baace811b1bd9ba9
MAPU mapu <3062278221@qq.com>
2023-04-19 20:40:44 +0800

    feat: 新增视图目录,新增菜单和添加视图

--- src/main/java/Setting.java
+++ src/main/java/Setting.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.sql.*;
+
 public class Setting {
     public static String[] getMySql() {
         String jdbcDriver = "com.mysql.cj.jdbc.Driver";
@@ -12,5 +14,22 @@ public class Setting {
         mysqlSet[2] = user;
         mysqlSet[3] = password;
         return mysqlSet;
+    }
+
+    public static Connection conMySql() {
+        String jdbcDriver = "com.mysql.cj.jdbc.Driver";
+        String dbUrl = "jdbc:mysql://121.40.48.93/bookManage";
+        String user = "abc";
+        String password = "123456";
+        Connection conn = null;
+        try {
+            Class.forName(jdbcDriver);
+            conn = DriverManager.getConnection(dbUrl, user, password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
     }
 }
